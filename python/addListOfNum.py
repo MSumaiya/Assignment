a6243e46dca923f8c7558c74d479df217853f7a8
MSumaiya <44896936+MSumaiya@users.noreply.github.com>
2018-11-11 16:29:28 +0100

    Add files via upload

---
+++ python/addListOfNum.py
@@ -0,0 +1,7 @@
+numbers = [1,2,3,5]
+def sumOfList(sumlist):
+  sum = 0
+  for n in sumlist:
+    sum +=n
+  return sum
+print(sumOfList(numbers))
---
+++ python/addTwoNum.py
@@ -0,0 +1,3 @@
+def add(a,b):
+    return int(a) + int(b)
+print(add(1,3))
